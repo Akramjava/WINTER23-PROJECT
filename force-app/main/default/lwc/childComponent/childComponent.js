8e3c06ab43294231a10a44d53ce2344ea67fe43c
Akramjava <akramttaa@gmail.com>
2023-02-17 20:15:03 -0500

    first commit

---
+++ force-app/main/default/lwc/childComponent/childComponent.js
@@ -0,0 +1,13 @@
+import { LightningElement, track, api } from 'lwc';
+
+export default class ChildComponent extends LightningElement {
+
+    @api stepValue;
+    get stepMassage() {
+        if (this.stepValue == '1') {
+            return 'Message 1';
+        } else if (this.stepValue == '2') {
+            return 'Message 2';
+        }
+    }
+}
